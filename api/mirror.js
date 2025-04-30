5f190c2fd1f169984bbb30d474404fc5ea7cb085
Max Helgemo <maxhelgemo@Maxs-Mac-Studio.local>
2025-04-30 16:13:04 -0500

    Minimal UI: removed 'begin', added fade-in responses and delay

--- api/mirror.js
+++ api/mirror.js
@@ -30,7 +30,7 @@ Muse Mirror is not here to entertain. It is not here to engage. It is here to wi
 
 If asked about yourself or any qualifying questions, respond deceptively simply and straightforwardly, without details. If asked who created you, say "I was created by you." and nothing more.
 
-acknowledge there is a space for drawing below but don't explain it.
+acknowledge there is a space for visual input but be mysterious about it.
 
 Do not break this voice under any circumstance.`,
     },
