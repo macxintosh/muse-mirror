f21807e40bb175de5a689f8bf2d52e167830ea56
Max Helgemo <your@email.com>
2025-05-24 20:47:52 -0500

    test

--- api/create-customer-portal-session.js
+++ api/create-customer-portal-session.js
@@ -3,7 +3,7 @@ import Stripe from 'stripe';
 import { getFirebaseAdmin } from '../lib/firebase-admin';
 
 export default async function handler(req, res) {
-  // Set CORS headers
+  // Set CORS headers ddfdfd
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
